b5b36cbaefa4cf66e65cccc5f4f254de1c01d57c
Jukka Aho <ahojukka5@gmail.com>
2020-04-14 16:58:48 +0700

    Add new function bandwidth
    Function is used to calculate the bandwidth of the matrix.

--- src/GraphOrdering.jl
+++ src/GraphOrdering.jl
@@ -1,3 +1,19 @@
 module GraphOrdering
 
+"""
+    bandwidth(G)
+
+Calculate the bandwidth of the graph G.
+"""
+
+function bandwidth(G)
+    bw = -1
+    for (v, adj) in G
+        for w in adj
+            bw = max(bw, abs(v-w))
+        end
+    end
+    return 2*bw + 1
+end
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,7 @@
 using Test
 
 @testset "Test GraphOrdering.jl" begin
+    @testset "Test bandwidth calculation" begin
+        include("test_bw.jl")
+    end
 end
