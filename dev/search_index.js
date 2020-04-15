e0858c757f41877135c52f66b9344de9c04c8e5b
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-04-15 17:31:29 +0000

    build based on f3b3cde

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#GraphOrdering.jl-1","page":"GraphOrdering.jl","title":"GraphOrdering.jl","text":"","category":"section"},{"location":"#","page":"GraphOrdering.jl","title":"GraphOrdering.jl","text":"Package author: Jukka Aho (@ahojukka5)","category":"page"},{"location":"#","page":"GraphOrdering.jl","title":"GraphOrdering.jl","text":"GraphOrdering.jl is a package focused on a problem of renumbering vertices of a graph such a way that the bandwidth of the sparse matrix is minimized.","category":"page"}]
+}
